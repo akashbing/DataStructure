28f9773e91d97990d118936c0c8b243e5fe1d9a0
akash <2011akashsoni@gmail.com>
2019-10-02 22:59:41 +0530

    alpha numeri linear seacrh

---
+++ linear search alpha numeric.py
@@ -0,0 +1,14 @@
+
+arr=[1,2,"akash",3,4,"soni",67,65]
+n="akash"
+
+
+def search_alpha(arr,n):
+     for i in range(len(arr)):
+         if arr[i]==n:
+            return True
+     return False
+if search_alpha(arr,n):
+    print("Found")
+else:
+    print("not found")
