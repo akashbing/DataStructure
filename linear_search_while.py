198e053c5441a5ed1283afd3ec32cb1b4db1a5de
akash <2011akashsoni@gmail.com>
2019-10-02 22:56:01 +0530

    linear search while

---
+++ linear_search_while.py
@@ -0,0 +1,15 @@
+arr=[1,2,3,4,5,66,343,44,7,8]
+
+n=8
+
+i=flag=0
+
+while i < len(arr):
+    if arr[i]==n:
+        flag=1
+        break
+    i=i+1
+if flag ==1:
+    print("item found in position",i)
+else:
+    print("item not found")
